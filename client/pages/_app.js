5f8fe42cbc60cfd382b114d91c44137f0b146b52
Noppadon Tongtrakoon <noppadon@enres.co>
2020-08-07 17:18:11 +0700

    222-passing props in _app.js

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,20 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import buildClient from '../api/build-client';
 
-const AppComponent = ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (<div>
-            <h1>Header !</h1>
+            <h1>Header {currentUser.email} !</h1>
             <Component {...pageProps} />
           </div>
           );
 };
 
 AppComponent.getInitialProps = async appContent => {
+  console.log(appContent);
+
   const client = buildClient(appContent.ctx);
   const { data } = await client.get('/api/users/currentuser');
 
-  console.log(data);
-  return data;
+  let pageProps;
+  if (appContent.Component.getInitialProps) {
+    pageProps = await appContent.Component.getInitialProps(appContent.ctx);
+  }
+
+  console.log("pageProps", pageProps);
+  return {
+    pageProps,
+    ...data
+  };
 };
 
 export default AppComponent;
