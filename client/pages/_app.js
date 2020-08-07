2caf463534c04033efd4b100041d221c5987472b
Noppadon Tongtrakoon <noppadon@enres.co>
2020-08-07 17:06:53 +0700

    219-moving getInitialProps()

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,9 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-export default ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps }) => {
   return (<div>
             <h1>Header !</h1>
             <Component {...pageProps} />
           </div>
           );
 };
+
+AppComponent.getInitialProps = async appContent => {
+  const client = buildClient(appContent.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  console.log(data);
+  return data;
+};
+
+export default AppComponent;
